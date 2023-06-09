e92b5dcec82bbcd311061e8a3c75008a03376c72
KamilOsypiuk <rejvenshieldpl001@gmail.com>
2023-06-09 15:06:52 +0200

    added users app with custom user

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+
 from .models import User
 
 # Register your models here.
@@ -8,7 +9,18 @@ from .models import User
 class CustomUserAdmin(UserAdmin):
     model = User
     ordering = ['email']
-    list_display = ['email', 'first_name', 'last_name', 'date_of_birth']
+    list_display = ['email', 'first_name', 'last_name', 'date_of_birth', 'phone_number']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('date_of_birth', 'phone_number',)}),
+        ('Permissions', {'fields': ('is_staff',)}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'date_of_birth', 'password1', 'password2'),
+        }),
+    )
 
 
 admin.site.register(User, CustomUserAdmin)
